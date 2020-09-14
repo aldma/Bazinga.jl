78387d402043b6a85c454036399514873860ecd4
Alberto De Marchi <albertodm92@libero.it>
2020-09-14 16:58:36 +0200

    added plots

--- test/data/plot_data.jl
+++ test/data/plot_data.jl
@@ -1,27 +1,18 @@
 using CSV
 using DataFrames, Query
-using Plots
+using PyPlot, PyCall
 
 ipopt6_filename = "/home/alberto/Documents/Bazinga.jl/test/data/cutest_ipopt_6.csv"
 alpx6__filename = "/home/alberto/Documents/Bazinga.jl/test/data/cutest_alpx__6.csv"
 ipopt8_filename = "/home/alberto/Documents/Bazinga.jl/test/data/cutest_ipopt_8.csv"
 alpx8__filename = "/home/alberto/Documents/Bazinga.jl/test/data/cutest_alpx__8.csv"
 
-df_ipopt = CSV.File(ipopt_filename) |> DataFrame
-df_alpx  = CSV.File(alpx__filename) |> DataFrame
+stats = Dict( :ALPX6 => CSV.File(alpx6__filename) |> DataFrame,
+              :IPOPT6 => CSV.File(ipopt6_filename) |> DataFrame,
+              :ALPX8 => CSV.File(alpx8__filename) |> DataFrame,
+              :IPOPT8 => CSV.File(ipopt8_filename) |> DataFrame, )
 
-stats = Dict( :ALPX__6 => CSV.File(alpx6__filename) |> DataFrame,
-              :IPOPT_6 => CSV.File(ipopt6_filename) |> DataFrame,
-              #:ALPX__8 => CSV.File(alpx8__filename) |> DataFrame,
-              #:IPOPT_8 => CSV.File(ipopt8_filename) |> DataFrame,
-            )
-
-
-
-
-
-cost(df) = (df.status .!= "first_order") * Inf + df.time
-
+cost(df) = df.time + (df.status .!= "first_order") * Inf
 solvers = keys(stats)
 
 statuses = []
@@ -32,7 +23,6 @@ end
 statuses = unique(statuses)
 
 tabs = DataFrame()
-
 for s in solvers
     println("==========  ",s,"  ==========")
     df = stats[s]
@@ -43,10 +33,44 @@ for s in solvers
     end
 end
 
-dfs = (stats[s] for s in solvers)
-P = hcat([cost(df) for df in dfs]...)
-logP = log10.(P)
-histogram(logP, label=hcat([string(s) for s in solvers]...), fillalpha=0.8)
+#dfs = (stats[s] for s in solvers)
+#P = hcat([cost(df) for df in dfs]...)
+#logP = log10.(P)
+#histogram(logP, label=hcat([string(s) for s in solvers]...), fillalpha=0.8)
 
-# using BenchmarkProfiles
+solvedIPOPT6 = (stats[:IPOPT6].status .== "first_order")
+solvedALPX6  = (stats[:ALPX6].status  .== "first_order")
+solvedBOTH6  = solvedALPX6 .& solvedIPOPT6
+solvedIPOPT8 = (stats[:IPOPT8].status .== "first_order")
+solvedALPX8  = (stats[:ALPX8].status  .== "first_order")
+solvedBOTH8  = solvedALPX8 .& solvedIPOPT8
+
+timeIPOPT6 = stats[:IPOPT6].time[solvedBOTH6]
+timeALPX6  = stats[:ALPX6].time[solvedBOTH6]
+timeIPOPT8 = stats[:IPOPT8].time[solvedBOTH8]
+timeALPX8  = stats[:ALPX8].time[solvedBOTH8]
+
+nIPOPT6 = sum( timeIPOPT6 .< timeALPX6 )
+nALPX6  = sum( timeIPOPT6 .> timeALPX6 )
+println("tol 6 : IPOPT ", nIPOPT6," - ", nALPX6," ALPX")
+nIPOPT8 = sum( timeIPOPT8 .< timeALPX8 )
+nALPX8  = sum( timeIPOPT8 .> timeALPX8 )
+println("tol 8 : IPOPT ", nIPOPT8," - ", nALPX8," ALPX")
+
+ratios6 = timeIPOPT6 ./ timeALPX6
+ratios8 = timeIPOPT8 ./ timeALPX8
+
+figure()
+hist( log10.(max.(1e-6,ratios6)), bins=20, histtype="step", label="ϵ = 1e-6 : IPOPT ($nIPOPT6) / ALPX ($nALPX6)", linewidth=2 )
+hist( log10.(max.(1e-6,ratios8)), bins=20, histtype="step", label="ϵ = 1e-8 : IPOPT ($nIPOPT8) / ALPX ($nALPX8)", linewidth=2 )
+legend(loc="upper left")
+xlabel("log10(time ratio)")
+xlim(-4,2)
+ymin, ymax = ylim()
+vlines(0,ymin,ymax)
+ylim(ymin,ymax)
+gcf()
+savefig("/home/alberto/Documents/Bazinga.jl/test/data/cutest_time_ratio_hist.pdf")
+
+#using BenchmarkProfiles
 #performance_profile(P, string.(solvers))
